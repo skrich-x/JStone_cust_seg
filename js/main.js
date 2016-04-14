b9e1e837618f9e48ee8ebc00e5eb302c2c64b9e3
Kyle Richardson <kylerichardson@Kyles-MacBook-Pro-2.local>
2016-04-13 22:39:27 -0400

    saving all new customer information to firebase

--- js/main.js
+++ js/main.js
@@ -1,8 +1,41 @@
-var fireUrl = "https://luminous-inferno-3512.firebaseio.com/";
+var fireUrl ="https://customer-info.firebaseio.com/";
 var newCustomer = new Firebase(fireUrl);
 
 $("#customer_form").submit(function(event){
       event.preventDefault();
+      var branch = $("#branch_name").val();
+      var companyName = $("#company_name").val();
+      var accountNumber = $("#account_number").val();
       var firstName = $("#first_name").val();
-      newCustomer.push({FirstName: firstName});
+      var lastName = $("#last_name").val();
+      var customerTitle = $("#customer_title").val();
+      var customerEmail = $("#customer_email").val();
+      var customerOfficePhone = $("#customer_office_phone").val();
+      var customerMobilePhone = $("#customer_mobile_phone").val();
+      var companyDescription = $("#company_description").val();
+      var companyTechs = $("#company_techs").val();
+      var companyPrimary = $("#company_primary").val();
+      var companySecondary = $("#company_secondary").val();
+      var newConstruction = $("#new_construction").val();
+      var retrofit = $("#retrofit").val();
+      var repair = $("#repair").val();
+
+      newCustomer.push({
+        Branch: branch,
+        Company: companyName,
+        Account: accountNumber,
+        FirstName:firstName,
+        LastName: lastName,
+        Title: customerTitle,
+        Email: customerEmail,
+        Office: customerOfficePhone,
+        Mobile: customerMobilePhone,
+        Description: companyDescription,
+        Techs: companyTechs,
+        PrimaryFocus: companyPrimary,
+        SecodaryFocus: companySecondary,
+        NewConstruction: newConstruction,
+        Retrofit: retrofit,
+        Repair: repair
+      });
 });
